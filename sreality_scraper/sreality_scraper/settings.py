08c5c2158d4e5043d25ff86bcb53603198aa1a0f
JakobGospodaric1 <jg9837@student.uni-lj.si>
2023-09-30 15:33:46 +0200

    scrapy finished

--- sreality_scraper/sreality_scraper/settings.py
+++ sreality_scraper/sreality_scraper/settings.py
@@ -12,12 +12,27 @@ BOT_NAME = "sreality_scraper"
 SPIDER_MODULES = ["sreality_scraper.spiders"]
 NEWSPIDER_MODULE = "sreality_scraper.spiders"
 
+# Splash Setuo
+# SPLASH_URL = 'http://localhost:8050'
+# DOWNLOADER_MIDDLEWARES = {
+#     'scrapy_splash.SplashCookiesMiddleware': 723,
+#     'scrapy_splash.SplashMiddleware': 725,
+#     'scrapy.downloadermiddlewares.httpcompression.HttpCompressionMiddleware': 810,
+# }
+
+# SPIDER_MIDDLEWARES = {
+#     'scrapy_splash.SplashDeduplicateArgsMiddleware': 100,
+# }
+
+# DUPEFILTER_CLASS = 'scrapy_splash.SplashAwareDupeFilter'
+
+# HTTPCACHE_STORAGE = 'scrapy_splash.SplashAwareFSCacheStorage'
 
 # Crawl responsibly by identifying yourself (and your website) on the user-agent
 #USER_AGENT = "sreality_scraper (+http://www.yourdomain.com)"
 
 # Obey robots.txt rules
-ROBOTSTXT_OBEY = True
+ROBOTSTXT_OBEY = False
 
 # Configure maximum concurrent requests performed by Scrapy (default: 16)
 #CONCURRENT_REQUESTS = 32
@@ -25,7 +40,7 @@ ROBOTSTXT_OBEY = True
 # Configure a delay for requests for the same website (default: 0)
 # See https://docs.scrapy.org/en/latest/topics/settings.html#download-delay
 # See also autothrottle settings and docs
-#DOWNLOAD_DELAY = 3
+DOWNLOAD_DELAY = 3
 # The download delay setting will honor only one of:
 #CONCURRENT_REQUESTS_PER_DOMAIN = 16
 #CONCURRENT_REQUESTS_PER_IP = 16
--- sreality_scraper/sreality_scraper/spiders/sreality.py
+++ sreality_scraper/sreality_scraper/spiders/sreality.py
@@ -1,10 +1,21 @@
 import scrapy
 
-
 class SrealitySpider(scrapy.Spider):
     name = "sreality"
     allowed_domains = ["sreality.cz"]
-    start_urls = ["https://sreality.cz"]
+    start_urls = ["https://www.sreality.cz/api/en/v2/estates?category_main_cb=1&category_type_cb=1&per_page=500"]
+
+    custom_settings = {
+        "ROBOTSTXT_OBEY": False,
+        "DOWNLOAD_DELAY": 0.5,
+    }
 
     def parse(self, response):
-        pass
+        resp = response.json()
+
+        for estate in resp["_embedded"]["estates"]:
+            yield {
+                "title": estate["name"],
+                "images": estate["_links"]["images"],
+            }
+            
